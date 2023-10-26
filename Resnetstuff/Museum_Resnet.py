1231e9476ba7e7ec6105441880f899ad4f0d249d
Anton Ickler <Anton.Ickler@web.de>
2023-10-27 00:01:44 +0200

    removed test config

--- Resnetstuff/Museum_Resnet.py
+++ Resnetstuff/Museum_Resnet.py
@@ -56,8 +56,8 @@ if __name__ == '__main__':
 
     print("Creating train, val, test dfs...")
 
-    train_df = important[important['Subset'] == 'train'][:1]
-    val_df = important[important['Subset'] == 'val'][:1]
+    train_df = important[important['Subset'] == 'train']
+    val_df = important[important['Subset'] == 'val']
     test_df = important[important['Subset'] == 'test']
 
     train_df = train_df.reset_index(drop=True)
