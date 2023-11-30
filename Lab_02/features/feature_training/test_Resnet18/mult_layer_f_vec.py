5d83d5333cc1d868101056cd3754ff7a956867a0
Anton Ickler <Anton.Ickler@web.de>
2023-11-30 12:25:22 +0100

    reports

--- Lab_02/features/feature_training/test_Resnet18/mult_layer_f_vec.py
+++ Lab_02/features/feature_training/test_Resnet18/mult_layer_f_vec.py
@@ -3,46 +3,109 @@ import pickle
 import numpy as np
 import tensorflow as tf
 import cv2
-import torch
-import torchvision
-from torch.utils.data import DataLoader
-from torchvision.transforms import transforms
 
-from cdataset import CustomDataset
-device = "cuda"
 
-def full_network_embedding(model, df, batch_size):
-    model.to(device)
+def full_network_embedding(model, image_paths, batch_size, target_layer_names, input_reshape, stats=None):
+    '''
+    Generates the Full-Network embedding[1] of a list of images using a pre-trained
+    model (input parameter model) with its computational graph loaded. Tensors used
+    to compose the FNE are defined by target_tensors input parameter. The input_tensor
+    input parameter defines where the input is fed to the model.
 
-    val_transform = torchvision.transforms.Compose([
-        transforms.Resize((256, 256)),
-        torchvision.transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-    val_dataset = CustomDataset(df, val_transform)
-    val_loader = DataLoader(df, batch_size=batch_size, shuffle=False)
+    By default, the statistics used to standardize are the ones provided by the same
+    dataset we wish to compute the FNE for. Alternatively these can be passed through
+    the stats input parameter.
 
-    model.eval()
-    # Turn on inference mode
-    with torch.no_grad():
-        for X, y in val_dataset:
-            # Send data to target device
-            #TODO: kein plan wie man das macht und kein Bock mehr :D
+    This function aims to generate the Full-Network embedding in an illustrative way.
+    We are aware that it is possible to integrate everything in a tensorflow operation,
+    however this is not our current goal.
 
-            X = torch.Tensor(X)
-            y = torch.Tensor(y)
-            y = y.long()
-            X, y = X.to(device), y.to(device)
+    [1] https://arxiv.org/abs/1705.07706
 
-            # 1. Forward pass
-            val_pred = model(X)  # raw logits
-            print(type(val_pred))
+    Args:
+        model (tf.GraphDef): Serialized TensorFlow protocol buffer (GraphDef) containing the pre-trained model graph
+                             from where to extract the FNE. You can get corresponding tf.GraphDef from default Graph
+                             using `tf.Graph.as_graph_def`.
+        image_paths (list(str)): List of images to generate the FNE for.
+        batch_size (int): Number of images to be concurrently computed on the same batch.
+        target_layer_names (list(str)): List of tensor names from model to extract features from.
+        input_reshape (tuple): A tuple containing the desired shape (height, width) used to resize the image.
+        stats (2D ndarray): Array of feature-wise means and stddevs for standardization.
+
+    Returns:
+       2D ndarray : List of features per image. Of shape <num_imgs,num_feats>
+       2D ndarry: Mean and stddev per feature. Of shape <2,num_feats>
+    '''
+
+    # Define feature extractor
+    feature_extractor = tf.keras.Model(
+        inputs=model.inputs,
+        outputs=[layer.output for layer in model.layers if layer.name in target_layer_names],
+    )
+    get_raw_features = lambda x: [tensor.numpy() for tensor in feature_extractor(x)]
+
+    # Prepare output variable
+    feature_shapes = [layer.output_shape for layer in model.layers if layer.name in target_layer_names]
+    len_features = sum(shape[-1] for shape in feature_shapes)
+    features = np.empty((len(image_paths), len_features))
+
+    # Extract features
+    for idx in range(0, len(image_paths), batch_size):
+        batch_images_path = image_paths[idx:idx + batch_size]
+        img_batch = np.zeros((len(batch_images_path), *input_reshape, 3), dtype=np.float32)
+        for i, img_path in enumerate(batch_images_path):
+            cv_img = cv2.imread(img_path)
+            try:
+                cv_img_resize = cv2.resize(cv_img, input_reshape)
+                img_batch[i] = np.asarray(cv_img_resize, dtype=np.float32)[:, :, ::-1]
+            except:
+                print(img_path)
+
+        feature_vals = get_raw_features(img_batch)
+        features_current = np.empty((len(batch_images_path), 0))
+        for feat in feature_vals:
+            # If its not a conv layer, add without pooling
+            if len(feat.shape) != 4:
+                features_current = np.concatenate((features_current, feat), axis=1)
+                continue
+            # If its a conv layer, do SPATIAL AVERAGE POOLING
+            pooled_vals = np.mean(np.mean(feat, axis=2), axis=1)
+            features_current = np.concatenate((features_current, pooled_vals), axis=1)
+        # Store in position
+        features[idx:idx + len(batch_images_path)] = features_current.copy()
+
+    # STANDARDIZATION STEP
+    # Compute statistics if needed
+    if stats is None:
+        stats = np.zeros((2, len_features))
+        stats[0, :] = np.mean(features, axis=0)
+        stats[1, :] = np.std(features, axis=0)
+    # Apply statistics, avoiding nans after division by zero
+    features = np.divide(features - stats[0], stats[1], out=np.zeros_like(features), where=stats[1] != 0)
+    if len(np.argwhere(np.isnan(features))) != 0:
+        raise Exception('There are nan values after standardization!')
+    # DISCRETIZATION STEP
+    th_pos = 0.15
+    th_neg = -0.25
+    features[features > th_pos] = 1
+    features[features < th_neg] = -1
+    features[[(features >= th_neg) & (features <= th_pos)][0]] = 0
+
+    # # Store output
+    # outputs_path = '../outputs'
+    # if not os.path.exists(outputs_path):
+    #     os.makedirs(outputs_path)
+    # np.save(os.path.join(outputs_path, 'fne.npy'), features)
+    # np.save(os.path.join(outputs_path, 'stats.npy'), stats)
+
+    # Return
+    return features, stats
 
 
 import os
-
+import tensorflow as tf
 from sklearn.metrics import classification_report, confusion_matrix
-
+from tensorflow.keras.models import load_model
 
 if __name__ == '__main__':
     # This shows an example of calling the full_network_embedding method using
@@ -51,20 +114,218 @@ if __name__ == '__main__':
     # model is straightforward.
 
     # Load model
+    print(os.path, flush=True)
     img_width, img_height = 256, 256
-    model = torch.load('model_resnet18')
+    model = load_model('model.keras')
+    initial_model = load_model('model.keras')
 
+    # target_layer_names = ['block1_conv1', 'block1_conv2', 'block2_conv1', 'block2_conv2', 'block3_conv1',
+    #                      'block3_conv2',
+    #                      'block3_conv3', 'block4_conv1', 'block4_conv2', 'block4_conv3', 'block5_conv1',
+    #                      'block5_conv2',
+    #                      'block5_conv3', 'fc1', 'fc2']
+
+    all_layer_names = ['input_1',
+                       'conv1_pad',
+                       'conv1_conv',
+                       'conv1_bn',
+                       'conv1_relu',
+                       'pool1_pad',
+                       'pool1_pool',
+                       'conv2_block1_1_conv',
+                       'conv2_block1_1_bn',
+                       'conv2_block1_1_relu',
+                       'conv2_block1_2_conv',
+                       'conv2_block1_2_bn',
+                       'conv2_block1_2_relu',
+                       'conv2_block1_0_conv',
+                       'conv2_block1_3_conv',
+                       'conv2_block1_0_bn',
+                       'conv2_block1_3_bn',
+                       'conv2_block1_add',
+                       'conv2_block1_out',
+                       'conv2_block2_1_conv',
+                       'conv2_block2_1_bn',
+                       'conv2_block2_1_relu',
+                       'conv2_block2_2_conv',
+                       'conv2_block2_2_bn',
+                       'conv2_block2_2_relu',
+                       'conv2_block2_3_conv',
+                       'conv2_block2_3_bn',
+                       'conv2_block2_add',
+                       'conv2_block2_out',
+                       'conv2_block3_1_conv',
+                       'conv2_block3_1_bn',
+                       'conv2_block3_1_relu',
+                       'conv2_block3_2_conv',
+                       'conv2_block3_2_bn',
+                       'conv2_block3_2_relu',
+                       'conv2_block3_3_conv',
+                       'conv2_block3_3_bn',
+                       'conv2_block3_add',
+                       'conv2_block3_out',
+                       'conv3_block1_1_conv',
+                       'conv3_block1_1_bn',
+                       'conv3_block1_1_relu',
+                       'conv3_block1_2_conv',
+                       'conv3_block1_2_bn',
+                       'conv3_block1_2_relu',
+                       'conv3_block1_0_conv',
+                       'conv3_block1_3_conv',
+                       'conv3_block1_0_bn',
+                       'conv3_block1_3_bn',
+                       'conv3_block1_add',
+                       'conv3_block1_out',
+                       'conv3_block2_1_conv',
+                       'conv3_block2_1_bn',
+                       'conv3_block2_1_relu',
+                       'conv3_block2_2_conv',
+                       'conv3_block2_2_bn',
+                       'conv3_block2_2_relu',
+                       'conv3_block2_3_conv',
+                       'conv3_block2_3_bn',
+                       'conv3_block2_add',
+                       'conv3_block2_out',
+                       'conv3_block3_1_conv',
+                       'conv3_block3_1_bn',
+                       'conv3_block3_1_relu',
+                       'conv3_block3_2_conv',
+                       'conv3_block3_2_bn',
+                       'conv3_block3_2_relu',
+                       'conv3_block3_3_conv',
+                       'conv3_block3_3_bn',
+                       'conv3_block3_add',
+                       'conv3_block3_out',
+                       'conv3_block4_1_conv',
+                       'conv3_block4_1_bn',
+                       'conv3_block4_1_relu',
+                       'conv3_block4_2_conv',
+                       'conv3_block4_2_bn',
+                       'conv3_block4_2_relu',
+                       'conv3_block4_3_conv',
+                       'conv3_block4_3_bn',
+                       'conv3_block4_add',
+                       'conv3_block4_out',
+                       'conv4_block1_1_conv',
+                       'conv4_block1_1_bn',
+                       'conv4_block1_1_relu',
+                       'conv4_block1_2_conv',
+                       'conv4_block1_2_bn',
+                       'conv4_block1_2_relu',
+                       'conv4_block1_0_conv',
+                       'conv4_block1_3_conv',
+                       'conv4_block1_0_bn',
+                       'conv4_block1_3_bn',
+                       'conv4_block1_add',
+                       'conv4_block1_out',
+                       'conv4_block2_1_conv',
+                       'conv4_block2_1_bn',
+                       'conv4_block2_1_relu',
+                       'conv4_block2_2_conv',
+                       'conv4_block2_2_bn',
+                       'conv4_block2_2_relu',
+                       'conv4_block2_3_conv',
+                       'conv4_block2_3_bn',
+                       'conv4_block2_add',
+                       'conv4_block2_out',
+                       'conv4_block3_1_conv',
+                       'conv4_block3_1_bn',
+                       'conv4_block3_1_relu',
+                       'conv4_block3_2_conv',
+                       'conv4_block3_2_bn',
+                       'conv4_block3_2_relu',
+                       'conv4_block3_3_conv',
+                       'conv4_block3_3_bn',
+                       'conv4_block3_add',
+                       'conv4_block3_out',
+                       'conv4_block4_1_conv',
+                       'conv4_block4_1_bn',
+                       'conv4_block4_1_relu',
+                       'conv4_block4_2_conv',
+                       'conv4_block4_2_bn',
+                       'conv4_block4_2_relu',
+                       'conv4_block4_3_conv',
+                       'conv4_block4_3_bn',
+                       'conv4_block4_add',
+                       'conv4_block4_out',
+                       'conv4_block5_1_conv',
+                       'conv4_block5_1_bn',
+                       'conv4_block5_1_relu',
+                       'conv4_block5_2_conv',
+                       'conv4_block5_2_bn',
+                       'conv4_block5_2_relu',
+                       'conv4_block5_3_conv',
+                       'conv4_block5_3_bn',
+                       'conv4_block5_add',
+                       'conv4_block5_out',
+                       'conv4_block6_1_conv',
+                       'conv4_block6_1_bn',
+                       'conv4_block6_1_relu',
+                       'conv4_block6_2_conv',
+                       'conv4_block6_2_bn',
+                       'conv4_block6_2_relu',
+                       'conv4_block6_3_conv',
+                       'conv4_block6_3_bn',
+                       'conv4_block6_add',
+                       'conv4_block6_out',
+                       'conv5_block1_1_conv',
+                       'conv5_block1_1_bn',
+                       'conv5_block1_1_relu',
+                       'conv5_block1_2_conv',
+                       'conv5_block1_2_bn',
+                       'conv5_block1_2_relu',
+                       'conv5_block1_0_conv',
+                       'conv5_block1_3_conv',
+                       'conv5_block1_0_bn',
+                       'conv5_block1_3_bn',
+                       'conv5_block1_add',
+                       'conv5_block1_out',
+                       'conv5_block2_1_conv',
+                       'conv5_block2_1_bn',
+                       'conv5_block2_1_relu',
+                       'conv5_block2_2_conv',
+                       'conv5_block2_2_bn',
+                       'conv5_block2_2_relu',
+                       'conv5_block2_3_conv',
+                       'conv5_block2_3_bn',
+                       'conv5_block2_add',
+                       'conv5_block2_out',
+                       'conv5_block3_1_conv',
+                       'conv5_block3_1_bn',
+                       'conv5_block3_1_relu',
+                       'conv5_block3_2_conv',
+                       'conv5_block3_2_bn',
+                       'conv5_block3_2_relu',
+                       'conv5_block3_3_conv',
+                       'conv5_block3_3_bn',
+                       'conv5_block3_add',
+                       'conv5_block3_out',
+                       'avg_pool',
+                       'predictions']
+    target_layer_names = [
+        'predictions',
+        "conv5_block3_out",
+        "conv5_block3_3_conv",
+        "conv5_block3_2_conv",
+        "conv5_block3_1_conv",
+        "conv5_block2_3_conv",
+        "conv5_block3_1_conv",
+        "conv5_block1_out",
+        "conv4_block6_out",
+        "conv4_block4_out",
+        "conv2_block2_out"
+    ]
 
     import pandas as pd
 
-    dataset = pd.read_csv("./../../../../../DataMeta/MAMe_dataset.csv")
-    labels = pd.read_csv("./../../../../../DataMeta/MAMe_labels.csv", header=None)
-    toy_data = pd.read_csv("./../../../../../DataMeta/MAMe_toy_dataset.csv")
+    dataset = pd.read_csv("./../../DataMeta/MAMe_dataset.csv")
+    labels = pd.read_csv("./../../DataMeta/MAMe_labels.csv", header=None)
+    toy_data = pd.read_csv("./../../DataMeta/MAMe_toy_dataset.csv")
 
     important = dataset[["Image file", "Subset", "Medium"]]
     important = important.rename(columns={"Medium": "label"})
     important = important.rename(columns={"Image file": "file_path"})
-    important["file_path"] = important["file_path"].apply(lambda x: "./../../../../../DataProcessed/data_256/" + str(x))
+    important["file_path"] = important["file_path"].apply(lambda x: "./../../DataProcessed/data_256/" + str(x))
 
     print("Mapping labels...")
     label_mapper = labels.to_dict()[1]
@@ -104,27 +365,39 @@ if __name__ == '__main__':
 
     print('Total train images:', len(train_images), ' with their corresponding', len(train_labels), 'labels')
     # Parameters for the extraction procedure
-    batch_size = 20
+    batch_size = 1
     input_reshape = (256, 256)
     # Call FNE method on the train set
-    fne_features = full_network_embedding(model, train_df, batch_size)
+    fne_features, fne_stats_train = full_network_embedding(initial_model, train_images, batch_size,
+                                                           target_layer_names, input_reshape)
     print('Done extracting features of training set. Embedding size:', fne_features.shape)
 
     from sklearn import svm
 
     # Train SVM with the obtained features.
     train_features = fne_features
-
+    clf = svm.LinearSVC()
+    clf.fit(X=train_features, y=train_labels)
 
     print('Done training SVM on extracted features of training set')
 
-    fne_features = full_network_embedding(model, val_images, batch_size)
+    fne_features, fne_stats_train = full_network_embedding(initial_model, val_images, batch_size,
+                                                           target_layer_names, input_reshape, stats=fne_stats_train)
     print('Done extracting features of val set')
 
     # Test SVM with the test set.
     val_features = fne_features
+    predicted_labels = clf.predict(fne_features)
     print('Done testing SVM on extracted features of test set')
 
+    # Print results
+    report = classification_report(val_labels, predicted_labels, output_dict=True)  #
+    df = pd.DataFrame(report).transpose()
+    df.to_csv("report_svm")
+
+    print(classification_report(val_labels, predicted_labels))
+    cm = confusion_matrix(val_labels, predicted_labels)
+    print(cm)
 
     with open('train_features.pkl', 'wb') as f:
         pickle.dump(train_features, f)
@@ -138,9 +411,18 @@ if __name__ == '__main__':
     with open('val_labels.pkl', 'wb') as f:
         pickle.dump(val_labels, f)
 
+    import matplotlib.pyplot as plt
+    from sklearn.metrics import confusion_matrix
+    import seaborn as sns
+
+    plt.figure(figsize=(10, 7))
+    sns.heatmap(cm, annot=True, fmt='d')
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+
+    # Saving the plot as a PNG file
+    plt.savefig('confusion_matrix.png')
+
     import try_models
-
-
-
-
-
+    print("Fertigggg")
--- Lab_02/finetuning_ResNet18/save_model.py
+++ Lab_02/finetuning_ResNet18/save_model.py
@@ -1,6 +1,14 @@
+import keras
 import torchvision.models as models
 import torch
 
-model = models.resnet18(pretrained=True)
-#model = torch.hub.load('pytorch/vision:v0.10.0', 'densenet201', pretrained=True)
-torch.save(model, "densenet201")
+#model = models.resnet18(pretrained=True)
+#model = torch.hub.load('pytorch/vision:v0.10.0', 'densenet201', pretrained=True)m
+model = keras.applications.ResNet50(
+    include_top=False,
+    weights="imagenet",
+    input_shape=(256, 256, 3),
+    classes=1000,
+    classifier_activation="softmax",
+)
+model.save("./model.h5")
